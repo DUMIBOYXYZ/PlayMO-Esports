6c90a61485d1bae673b20225ef23424391e85bfa
DUMI BOY <dumidu2005max@gmail.com>
2026-06-27 17:57:33 +0530

    Updated live chat and project files

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <main style={{ padding: 20, textAlign: "center" }}>
-      <h1>🏆 NETFLIX MOVIES FREE </h1>
+      <h1>🏆 FIFA SERVER </h1>
 
       <iframe
         width="100%"
         height="2000"
-        src="https://movies2watch.biz/"
+        src="https://fan24.pk/"
         allow="autoplay; encrypted-media"
         allowFullScreen
       />
