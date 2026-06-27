709f32dbdc2346a9fa0a0e760c68266928b99a2f
DUMI BOY <dumidu2005max@gmail.com>
2026-06-27 17:39:45 +0530

    Updated live chat and project files

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <main style={{ padding: 20, textAlign: "center" }}>
-      <h1>🏆 FIFA 26 LIVE @dumiboy.xyz </h1>
+      <h1>🏆 NETFLIX MOVIES FREE </h1>
 
       <iframe
         width="100%"
-        height="500"
+        height="100%"
         src="https://movies2watch.biz/"
         allow="autoplay; encrypted-media"
         allowFullScreen
